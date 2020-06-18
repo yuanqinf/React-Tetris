f4849bc9cc42996da44c4c5326e6f345c3ef16c8
Yuanqin Fan <fanyuanqin96@gmail.com>
2020-06-18 23:19:35 +0800

    Add files via upload

---
+++ react-tetris/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Tetris from "./components/Tetris";
+
+function App() {
+  return (
+    <div className="App">
+      <Tetris />
+    </div>
+  );
+}
+
+export default App;
